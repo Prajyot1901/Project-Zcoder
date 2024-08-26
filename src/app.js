8ec3a2d86b6c725015a590444bf703b8997cd38b
prajyot pyati <prajyotpyati1@gmail.com>
2024-08-27 02:23:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app=express();
 app.set("trust proxy",1);
-app.use(cors({origin: ['http://127.0.0.1:5500', 'https://localhost:5500'], // List all allowed origins
+app.use(cors({origin: ['https://project-zcoder-api.vercel.app/'], // List all allowed origins
 methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 credentials: true, // Enable credentials for cross-origin requests
 allowedHeaders: 'Content-Type, Authorization',}));                           //change settings to allow request from front end
